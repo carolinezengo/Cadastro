af112025ace229f2cf1b414fb9ab22bd53044ddd
carolinezengo <carolinezengo@gmail.com>
2023-07-19 22:53:22 -0300

    Cadastro Formulario VB.NET

---
+++ Cadastro/Form1.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtcod = New TextBox()
+        Label6 = New Label()
+        txtnome = New TextBox()
+        Label7 = New Label()
+        txttel = New TextBox()
+        Label8 = New Label()
+        TextBox8 = New TextBox()
+        Label9 = New Label()
+        txtemail = New TextBox()
+        Label10 = New Label()
+        TextBox10 = New TextBox()
+        Label11 = New Label()
+        TextBox11 = New TextBox()
+        Label14 = New Label()
+        TextBox14 = New TextBox()
+        Label15 = New Label()
+        TextBox15 = New TextBox()
+        btnovo = New Button()
+        btnsalvar = New Button()
+        btnsair = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 36)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(29, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Cod"
+        ' 
+        ' txtcod
+        ' 
+        txtcod.Location = New Point(59, 36)
+        txtcod.Name = "txtcod"
+        txtcod.Size = New Size(100, 23)
+        txtcod.TabIndex = 1
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(12, 81)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(40, 15)
+        Label6.TabIndex = 8
+        Label6.Text = "Nome"
+        ' 
+        ' txtnome
+        ' 
+        txtnome.Location = New Point(59, 81)
+        txtnome.Name = "txtnome"
+        txtnome.Size = New Size(100, 23)
+        txtnome.TabIndex = 9
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(12, 125)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(21, 15)
+        Label7.TabIndex = 8
+        Label7.Text = "Tel"
+        ' 
+        ' txttel
+        ' 
+        txttel.Location = New Point(59, 125)
+        txttel.Name = "txttel"
+        txttel.Size = New Size(100, 23)
+        txttel.TabIndex = 9
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(-327, -32)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(41, 15)
+        Label8.TabIndex = 8
+        Label8.Text = "Label5"
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(-280, -32)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(100, 23)
+        TextBox8.TabIndex = 9
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(12, 170)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(36, 15)
+        Label9.TabIndex = 8
+        Label9.Text = "Email"
+        ' 
+        ' txtemail
+        ' 
+        txtemail.Location = New Point(59, 170)
+        txtemail.Name = "txtemail"
+        txtemail.Size = New Size(100, 23)
+        txtemail.TabIndex = 9
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(-327, 57)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(41, 15)
+        Label10.TabIndex = 8
+        Label10.Text = "Label5"
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(-280, 57)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(100, 23)
+        TextBox10.TabIndex = 9
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(-666, -100)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(41, 15)
+        Label11.TabIndex = 8
+        Label11.Text = "Label5"
+        ' 
+        ' TextBox11
+        ' 
+        TextBox11.Location = New Point(-619, -100)
+        TextBox11.Name = "TextBox11"
+        TextBox11.Size = New Size(100, 23)
+        TextBox11.TabIndex = 9
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Location = New Point(-307, 181)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(41, 15)
+        Label14.TabIndex = 8
+        Label14.Text = "Label5"
+        ' 
+        ' TextBox14
+        ' 
+        TextBox14.Location = New Point(-260, 181)
+        TextBox14.Name = "TextBox14"
+        TextBox14.Size = New Size(100, 23)
+        TextBox14.TabIndex = 9
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Location = New Point(-646, 24)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(41, 15)
+        Label15.TabIndex = 8
+        Label15.Text = "Label5"
+        ' 
+        ' TextBox15
+        ' 
+        TextBox15.Location = New Point(-599, 24)
+        TextBox15.Name = "TextBox15"
+        TextBox15.Size = New Size(100, 23)
+        TextBox15.TabIndex = 9
+        ' 
+        ' btnovo
+        ' 
+        btnovo.Location = New Point(12, 255)
+        btnovo.Name = "btnovo"
+        btnovo.Size = New Size(75, 23)
+        btnovo.TabIndex = 10
+        btnovo.Text = "Novo"
+        btnovo.UseVisualStyleBackColor = True
+        ' 
+        ' btnsalvar
+        ' 
+        btnsalvar.Location = New Point(113, 255)
+        btnsalvar.Name = "btnsalvar"
+        btnsalvar.Size = New Size(75, 23)
+        btnsalvar.TabIndex = 11
+        btnsalvar.Text = "Gravar"
+        btnsalvar.UseVisualStyleBackColor = True
+        ' 
+        ' btnsair
+        ' 
+        btnsair.Location = New Point(210, 255)
+        btnsair.Name = "btnsair"
+        btnsair.Size = New Size(75, 23)
+        btnsair.TabIndex = 12
+        btnsair.Text = "Sair"
+        btnsair.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(238, 44)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(211, 15)
+        Label2.TabIndex = 13
+        Label2.Text = "                                                                    "
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(238, 89)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(211, 15)
+        Label3.TabIndex = 14
+        Label3.Text = "                                                                    "
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(238, 183)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(211, 15)
+        Label4.TabIndex = 15
+        Label4.Text = "                                                                    "
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(238, 133)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(211, 15)
+        Label5.TabIndex = 16
+        Label5.Text = "                                                                    "
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(btnsair)
+        Controls.Add(btnsalvar)
+        Controls.Add(btnovo)
+        Controls.Add(TextBox15)
+        Controls.Add(TextBox11)
+        Controls.Add(TextBox8)
+        Controls.Add(txtnome)
+        Controls.Add(Label15)
+        Controls.Add(Label11)
+        Controls.Add(Label8)
+        Controls.Add(Label6)
+        Controls.Add(TextBox14)
+        Controls.Add(TextBox10)
+        Controls.Add(txttel)
+        Controls.Add(Label14)
+        Controls.Add(Label10)
+        Controls.Add(Label7)
+        Controls.Add(txtemail)
+        Controls.Add(Label9)
+        Controls.Add(txtcod)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Cadastro"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtcod As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtnome As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txttel As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtemail As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents Label14 As Label
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents btnovo As Button
+    Friend WithEvents btnsalvar As Button
+    Friend WithEvents btnsair As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+End Class
---
+++ Cadastro/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Dim cod As Integer
+    Dim nome As String
+    Dim tel As Double
+    Dim email As String
+
+    Private Sub btnovo_Click(sender As Object, e As EventArgs) Handles btnovo.Click
+        txtcod.Text = ""
+        txtnome.Text = ""
+        txttel.Text = ""
+        txtemail.Text = ""
+        Label2.Text = ""
+        Label3.Text = ""
+        Label4.Text = ""
+        Label5.Text = ""
+
+
+
+    End Sub
+
+    Private Sub btnsalvar_Click(sender As Object, e As EventArgs) Handles btnsalvar.Click
+        cod = txtcod.Text
+        nome = txtnome.Text
+        tel = txttel.Text
+        email = txtemail.Text
+        Label2.Text = cod
+        Label3.Text = nome
+        Label5.Text = tel
+        Label4.Text = email
+
+
+    End Sub
+
+    Private Sub btnsair_Click(sender As Object, e As EventArgs) Handles btnsair.Click
+
+        Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
